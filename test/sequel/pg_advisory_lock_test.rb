d3d846181c166825eaa0e8a2b315036649db7c9a
Yury Shchyhlinski <Shchyhlinski.YL@gmail.com>
2017-10-15 17:48:45 +0300

    Write spec stubs for PgAdvisoryLock#register_advisory_lock method

--- test/sequel/pg_advisory_lock_test.rb
+++ test/sequel/pg_advisory_lock_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
-class Sequel::Postgres::PgAdvisoryLockTest < Minitest::Test
-  def test_it_does_something_useful
-    assert false
+describe Sequel::Postgres::PgAdvisoryLock do
+
+  describe '#register_advisory_lock' do
+
+    it 'should register lock for all known PostgreSQL functions' do
+      assert false
+    end
+
+    it 'should prevent specifying invalid PostgreSQL function as lock type' do
+      assert false
+    end
+
+    it 'should prevent registering multiple locks with same name' do
+      assert false
+    end
+
+    it 'registered locks must have different lock keys' do
+      assert false
+    end
+
+    it 'mapping between lock name and lock key must be constant' do
+      assert false
+    end
   end
+
+  describe '#with_advisory_lock' do
+
+  end
+
 end
