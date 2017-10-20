0181309cda7d655d4b37149dbc6de086033fd1a5
Yury Shchyhlinski <Shchyhlinski.YL@gmail.com>
2017-10-21 01:58:14 +0300

    Setup database connection and write tests for #register_advisory_lock method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sequel/extensions/pg_advisory_lock'
 
 require 'minitest/autorun'
+require 'yaml'
+
+DB = Sequel.connect(ENV['PG_TEST_DB'] || 'postgres://localhost/postgres?user=postgres')
+DB.extension(:pg_advisory_lock)
